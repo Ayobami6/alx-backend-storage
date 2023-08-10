357c1b176b0f83472f7d149e405119335ab5140f
Ayobami <ayobamidele006@gmail.com>
2023-08-10 19:07:08 +0100

    fix user_id name

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,20 @@
+-- Active: 1691674992801@@127.0.0.1@3306@test_db
 -- stored procedure scripts that computes the average score of a student
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 DELIMITER $
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id 
+CREATE PROCEDURE ComputeAverageScoreForUser(IN usr_id
 INT) 
 BEGIN
 	UPDATE users
 	SET average_score = (
 	        SELECT AVG(score)
 	        FROM corrections
-	        WHERE user_id = user_id
+	        WHERE user_id = usr_id
 	    )
-	WHERE id = user_id;
+	WHERE id = usr_id;
 	END$ 
 
 
