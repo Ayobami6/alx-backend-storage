a4a9b9cae8810902b05461b46b0dd3ae6bc94da5
Ayobami <ayobamidele006@gmail.com>
2023-08-10 18:32:28 +0100

    update task 3 year in query

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- Active: 1691578679906@@127.0.0.1@5432@hbtn_0d_tvshows
+-- Active: 1691674992801@@127.0.0.1@3306@test_db
 
 -- list all bands with Glam rock as their main style, sorted by the number
 
@@ -6,7 +6,7 @@
 
 SELECT
     band_name,
-    IFNULL(split, 2020) - formed AS lifespan
+    IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
